4ad75a8f1861074b1b3528d67a928cd8c81e089b
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2023-09-26 14:03:43 +0200

    init zfe

---
+++ zfe/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { default as grapesjs } from 'grapesjs';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		let editor = grapesjs.init({
+			container: '#gjs'
+		});
+	});
+
+</script>
+
+<svelte:head>
+	<meta charset="UTF-8" />
+	<link rel="icon" type="image/svg+xml" href="/vite.svg" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Vite + TS</title>
+
+	<link
+		rel="stylesheet"
+		href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css"
+	/>
+	<link rel="stylesheet" href="//unpkg.com/grapesjs/dist/css/grapes.min.css" />
+	<link rel="stylesheet" href="https://grapesjs.com/stylesheets/grapesjs-preset-webpage.min.css" />
+	<link rel="stylesheet" href="https://grapesjs.com/stylesheets/tooltip.css" />
+	<link rel="stylesheet" href="https://grapesjs.com/stylesheets/demos.css?v3" />
+	<link href="https://unpkg.com/grapick/dist/grapick.min.css" rel="stylesheet" />
+</svelte:head>
+
+<div id="app">
+	<div id="gjs" />
+</div>
