11f392c16a7fb563c4b07abe636749475c55dd72
Övünç Yayla <6746986+ovuncyayla@users.noreply.github.com>
2023-09-27 07:25:22 +0200

    add login route

--- zfe/src/routes/login/+page.server.js
+++ zfe/src/routes/login/+page.server.js
@@ -1,8 +1,35 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect, fail } from '@sveltejs/kit';
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
 
 export const actions = {
-	default: ({ cookies, url }) => {
-		cookies.set('logged_in', 'true', { path: '/' });
-		throw redirect(303, url.searchParams.get('redirectTo') ?? '/');
+	default: async ({ cookies, url, request }) => {
+
+		const data = await request.formData();
+		
+	try {
+
+			const authData = await pb.admins.authWithPassword(data.get("email"), data.get("password"));
+			// const authData = await pb.collection("users").authWithPassword(data.get("email"), data.get("password"));
+			console.log(authData);
+
+			console.log(pb.authStore.isValid);
+			console.log(pb.authStore.token);
+			console.log(pb.authStore.model.id);
+
+		} catch (error) {
+			console.log(error);
+			return fail(400, { description: "Error while authenticating" })
+		}
+
+		if (pb.authStore.isValid) {
+			cookies.set('logged_in', 'true', { path: '/' });
+			cookies.set('toky', pb.authStore.token, { path: '/' });
+			throw redirect(303, url.searchParams.get('redirectTo') ?? '/');
+		}
+
+		return fail(403, { description: "Unauthorized" })
+
 	}
 };
--- zfe/src/routes/login/+page.svelte
+++ zfe/src/routes/login/+page.svelte
@@ -1,6 +1,93 @@
 <script>
+	export let data;
+	export let form;
 </script>
 
-<form method="POST">
-	<button>log in</button>
-</form>
+<main class="grid-3-coll">
+	<article class="main-center">
+	{#if form?.description}
+		<p class="error">{form.description}</p>
+	{/if}
+		<div>
+			<hgroup>
+				<h1>Login</h1>
+				<h2>Log me in</h2>
+			</hgroup>
+			<form method="POST">
+				<input
+					type="text"
+					name="email"
+					placeholder="Email"
+					aria-label="Login"
+					autocomplete="nickname"
+					required
+				/>
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+					aria-label="Password"
+					autocomplete="current-password"
+					required
+				/>
+				<!-- <fieldset> -->
+				<!-- 	<label for="remember"> -->
+				<!-- 		<input type="checkbox" role="switch" id="remember" name="remember" /> -->
+				<!-- 		Remember me -->
+				<!-- 	</label> -->
+				<!-- </fieldset> -->
+				<button type="submit" class="contrast">Login</button>
+			</form>
+		</div>
+	</article>
+</main>
+
+<style>
+	.grid-3-coll {
+		display: grid;
+		grid-template-columns: 0.6fr 1.8fr 0.6fr;
+		grid-template-rows: 0.4fr 2.3fr 0.3fr;
+		gap: 0px 0px;
+		grid-auto-flow: row;
+		grid-template-areas:
+			'head-begin head-center head-end'
+			'main-begin main-center main-end'
+			'tail-begin tail-center tail-end';
+	}
+
+	.head-center {
+		grid-area: head-center;
+	}
+
+	.main-center {
+		grid-area: main-center;
+	}
+
+	.head-begin {
+		grid-area: head-begin;
+	}
+
+	.head-end {
+		grid-area: head-end;
+	}
+
+	.main-begin {
+		grid-area: main-begin;
+	}
+
+	.main-end {
+		grid-area: main-end;
+	}
+
+	.tail-center {
+		grid-area: tail-center;
+	}
+
+	.tail-begin {
+		grid-area: tail-begin;
+	}
+
+	.tail-end {
+		grid-area: tail-end;
+	}
+</style>
